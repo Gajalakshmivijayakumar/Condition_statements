b2330d105fd2c1337af9a6d10f7153a4bd6fbcd6
Gajalakshmi <gajalakshmi.vijayakumar@jcu.edu.au>
2024-09-17 08:22:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo_program
+{
+    class If
+    {
+        static void Main(string[] args)
+        {
+            int hours = 10;
+            if (hours > 0 && hours <12)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            else if(hours >= 12 && hours < 18) 
+            {
+                Console.WriteLine("Good Evening!!");
+            }
+            else
+            {
+                Console.WriteLine("Good noon!!");
+            }
+                
+        }
+    }
+}
